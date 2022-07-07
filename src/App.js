ef3529ca2cdf904fcd16a1e5676515ae6574c29f
AlbertoMathew <listblack229@gmail.com>
2022-07-07 10:45:23 +0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alberto Mathew
         </a>
       </header>
     </div>
